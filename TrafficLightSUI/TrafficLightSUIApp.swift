30d5c6fc1cfd67e76b162c9d893b24c13dc3eb08
Bogdan Radchenko <inroph@mail.ru>
2023-04-21 18:15:01 +0300

    Initial Commit

---
+++ TrafficLightSUI/TrafficLightSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSUIApp.swift
+//  TrafficLightSUI
+//
+//  Created by Богдан Радченко on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
